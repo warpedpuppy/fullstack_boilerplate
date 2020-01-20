7c0096dce3af61293d3fc376b9faaaa6b21c27f1
ted <ted@warpedpuppy.com>
2020-01-20 10:09:43 -0500

    added site context

--- backend/src/testing/testing-router.js
+++ backend/src/testing/testing-router.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const testingRouter = express.Router();
+const jsonBodyParser = express.json();
+const { requireAuth } = require('../middleware/auth-middleware')
+const AuthService = require('../auth/auth-service');
 
 testingRouter
 .get("/", (req, res) => {
+    let jwt = AuthService.createJwt("testing", {testing: true})
+    res
+    .status(200)
+    .json({success: true, jwt})
+})
+.post("/protected", jsonBodyParser, requireAuth, (req, res) => {
     res
     .status(200)
     .json({success: true})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,29 @@ import Page2 from './pages/Page2';
 import NotFound from './pages/NotFound';
 import Menu from './components/Menu';
 import { Switch, Route } from 'react-router-dom';
+import SiteContext from './SiteContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header><Menu /></header>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/page2" component={Page2} />
-          <Route component={NotFound} />
-        </Switch>
-      <footer>this is the footer</footer>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loggedIn: false
+  }
+  render() {
+    const contextValue = {
+      loggedIn: this.state.loggedIn
+    };
+     return (
+      <SiteContext.Provider value={contextValue}>
+        <header><Menu /></header>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/page2" component={Page2} />
+            <Route component={NotFound} />
+          </Switch>
+        <footer>this is the footer</footer>
+      </SiteContext.Provider>
+    );
+  }
+ 
 }
 
 export default App;
