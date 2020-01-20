eb2fcb9e6e20a6543c5c088157e6231f5662953c
ted <ted@warpedpuppy.com>
2020-01-20 09:59:11 -0500

    added protected backend endpoint

--- frontend/src/services/apicall-service.js
+++ frontend/src/services/apicall-service.js
@@ -1,3 +1,4 @@
+import TokenService from '../services/token-services';
 export default {
     async TestAPICall(str) {
         let res = await fetch(str);
@@ -5,5 +6,24 @@ export default {
         return await ((!res.ok)
             ? res.json().then(e => Promise.reject(e))
             : res.json())
-    }
+    },
+    async TestProtectedAPICall(str) {
+        console.log(str)
+        let res = await fetch(str,
+        {
+            method: "POST", 
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${TokenService.getAuthToken()}`,
+            },
+            body: JSON.stringify({ testing: true }),
+        });
+
+        return await ((!res.ok)
+            ? res.json().then(e => Promise.reject(e))
+            : res.json())
+    },
+
+
+   
 }
